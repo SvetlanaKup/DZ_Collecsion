844756cfca49b6918718c5f53f4257a5603349c2
SvetlanaKup <svetakupriyanova@mail.ru>
2025-09-10 22:56:09 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
-package ДЗ_PowerfulSet;
+package ДЗ_Коллекции;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
-        Set<Integer> set2 = new HashSet<>(Arrays.asList(0, 1, 2, 4));
+        // Создаем пример списка с дублями
+        List<Integer> list = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+        System.out.println("Исходный список: " + list);
 
-        System.out.println("Пересечение: " + PowerfulSet.intersection(set1, set2));
-        System.out.println("Объединение: " + PowerfulSet.union(set1, set2));
-        System.out.println("Разность (set1 без set2): " + PowerfulSet.relativeComplement(set1, set2));
+        // Получаем уникальные элементы
+        Set<Integer> uniqueElements = CollectionUtils.getUniqueElements(list);
+
+        // Вывод результата
+        System.out.println("Уникальные элементы: " + uniqueElements);
     }
 }
